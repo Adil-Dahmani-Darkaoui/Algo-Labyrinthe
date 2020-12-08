671a446aee3d0134aaab1cb151e9c606781e7d65
adarkaoui <adarkaoui@oph74.fr>
2020-12-08 15:42:41 +0100

    ADD - create maze function

--- src/main.js
+++ src/main.js
@@ -1,14 +1,69 @@
-async function loadJson() {
+async function loadJson(size) {
+
     const data = await fetch('labyrinthes.json')
         .then(response => response.json());
 
-    for (let i = 0; i < data[3]['ex-0'].length; i++){
-        let element = document.createElement('div');
-        let currentDiv = document.getElementById('mainDiv');
-        currentDiv.setAttribute('style', 'background-color: orange');
-        currentDiv.appendChild(element)
-    }
-    console.log(data[3]['ex-0'])
+    let gridSize = size;
+    let mazeName = 'ex-1';
+
+    return {
+        gridSize: gridSize,
+        cellData: data[gridSize][mazeName]
+    };
 }
 
-console.log(loadJson());
+function createMaze(mazeBoard) {
+
+    // We destructure the object
+    const {cellData, gridSize} = mazeBoard;
+
+    // We select the element on which we will create the grid layout
+    const mainDiv = document.getElementById('mainDiv');
+    mainDiv.style.gridTemplateColumns = 'repeat(' + gridSize + ', 100px)';
+    mainDiv.style.gridTemplateRows = 'repeat(' + gridSize + ', 100px)';
+
+    // We loop trough to create cells and walls
+    for (let i = 0; i < cellData.length; i++) {
+
+        // We create a div for each iteration
+        let cell = document.createElement('div');
+
+        // We set the first cell in orange
+        if (i === 0) {
+            cell.style.backgroundColor = 'orange'
+        }
+
+        // We get the last iteration to set the cell in green
+        if (i === cellData.length - 1) {
+            cell.style.backgroundColor = "green"
+        }
+
+        // We apply the class for the color
+        cell.className = 'cell-color cell-' + i;
+
+        // We apply the border for the walls
+        let walls = cellData[i]["walls"];
+        if (walls[0]) {
+            cell.style.borderTop = '1px solid'
+        }
+        if (walls[1]) {
+            cell.style.borderRight = '1px solid'
+        }
+        if (walls[2]) {
+            cell.style.borderBottom = '1px solid'
+        }
+        if (walls[3]) {
+            cell.style.borderLeft = '1px solid'
+        }
+
+        mainDiv.appendChild(cell)
+    }
+}
+
+async function main() {
+    // Async main function to call our maze generator functions
+    let size = prompt("Choice a maze size between 3 to 25");
+    createMaze(await loadJson(size));
+}
+
+main();
